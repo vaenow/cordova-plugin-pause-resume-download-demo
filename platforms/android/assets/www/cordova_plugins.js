3af4e3bf94acac1d8bfd420aacdab4ee29d85b7c
vaenow <luowenbiz@gmail.com>
2016-07-20 01:13:49 +0800

    init PRD all

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-pause-resume-download/www/jsv2/PauseResumeDownload.js",
+        "id": "cordova-plugin-pause-resume-download.PauseResumeDownload",
+        "clobbers": [
+            "PRD"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-pause-resume-download": "1.0.1"
+};
+// BOTTOM OF METADATA
+});
---
+++ plugins/cordova-plugin-pause-resume-download/README.md
@@ -0,0 +1,7 @@
+
+# cordova-plugin-pause-resume-download
+Use the way Pause & Resume to download files for Cordova/PhoneGap
+
+扩展CordovaFilePlugin，断点续下载
+
+Thanks my friend @Jason @BlackSi
